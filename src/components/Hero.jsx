c05a8afe1e11f7a61f6d8e4e46b3cd6f1cd3edc0
Shakha <vedu.pokale2004@gmail.com>
2026-07-08 19:39:26 +0530

    Initial Setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero () {
+    return (
+        <div className="section bg-[#0F172A] text-[#EBEBEB] border-[#334155]">
+            Hero Section
+        </div>
+    )
+}
+
+export default Hero
