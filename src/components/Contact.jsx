7f1f4853dde3fefa8a38244600537d53029d3beb
Shakha <vedu.pokale2004@gmail.com>
2026-07-08 19:39:37 +0530

    Initial Setup

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+function Contact () {
+    return (
+        <div className="section bg-[#0F172A] text-[#EBEBEB] border-[#334155]">
+            Contact Section
+        </div>
+    )
+}
+
+export default Contact
