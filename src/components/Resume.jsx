a25cb219bc695cfc78a3d56c9f296fa6d69a035f
Shakha <vedu.pokale2004@gmail.com>
2026-07-08 19:37:39 +0530

    Initial Setup

---
+++ src/components/Resume.jsx
@@ -0,0 +1,9 @@
+function Resume () {
+    return (
+        <div className="section bg-[#FAF8FC] text-[#1E293B] border-[#E2E8F0]">
+            Resume Section
+        </div>
+    )
+}
+
+export default Resume
