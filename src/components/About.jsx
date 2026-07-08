4b34e0e2da1c44b02b03bfa7405646d76bb1d2b0
Shakha <vedu.pokale2004@gmail.com>
2026-07-08 19:40:35 +0530

    Initial Setup

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+function About () {
+    return (
+        <div className="section bg-[#FAF8FC] text-[#1E293B] border-[#E2E8F0]">
+            About Section
+        </div>
+    )
+}
+
+export default About
