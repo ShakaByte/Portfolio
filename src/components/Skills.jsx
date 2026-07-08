326e63a2851cceeef4e4e7ad997b916fc2ee7116
Shakha <vedu.pokale2004@gmail.com>
2026-07-08 19:37:25 +0530

    Initial Setup

---
+++ src/components/Skills.jsx
@@ -0,0 +1,9 @@
+function Skills () {
+    return (
+        <div className="section bg-[#0F172A] text-[#EBEBEB] border-[#334155]">
+            Skills Section
+        </div>
+    )
+}
+
+export default Skills
