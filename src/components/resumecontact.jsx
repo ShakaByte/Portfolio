35ad6e9bbefa95269fbd2642a9393ebd07915e9d
Shakha <vedu.pokale2004@gmail.com>
2026-08-04 17:04:17 +0530

    Update resumecontact.jsx

--- src/components/resumecontact.jsx
+++ src/components/resumecontact.jsx
@@ -1,14 +1,16 @@
 import Resume from './Resume'
 import Contact from './Contact'
+import Footer from './footer'
 
 function ResumeContact() {
   return (
-    <div className="section bg-slate-50 text-slate-800 px-4 md:px-24 h-screen flex flex-col justify-start md:justify-center pt-20 md:pt-0">
-      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-10">
-        <Resume />
-        <Contact />
-      </div>
-    </div>
+    <div id='ResumeContact' className="section bg-slate-50 text-slate-800 px-4 md:px-24 h-screen flex flex-col justify-start pt-20 pb-6">
+  <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-16 md:gap-24">
+    <Resume />
+    <Contact />
+  </div>
+  <Footer />
+</div>
   )
 }
 
