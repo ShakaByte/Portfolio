d1725a4dc9c709def9b63b1f96beb77d84776333
Shakha <vedu.pokale2004@gmail.com>
2026-08-04 17:44:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Certificates from './components/Certificates'
-import ResumeContact from './components/ResumeContact'
+import ResumeContact from './components/resumecontact'
 import Navbar from './components/navbar'
 
 import './App.css'
